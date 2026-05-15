c0f43b709345e3804fca9c96791af1d2292cff44
Klara <2605499236@qq.com>
2026-05-15 17:52:36 +0800

    feat(speed): pre-rendered colored digits + throttle color bar
    - 11-step colored digit bitmaps (Tixing blue-yellow-red gradient)
    - Generated via tools/gen_colored_digits.py with gamma-correct tinting
    - Color bar only visible in throttle mode, clears on exit
    - Throttle double-click exits to menu
    - 538KB flash for colored digit assets

---
+++ ridewind-esp/main/resources/colored_digits.h
@@ -0,0 +1,126 @@
+#pragma once
+#include <stdint.h>
+
+#define COLORED_DIGIT_STEPS 11
+
+extern const unsigned char gImage_speed_0_c0[5414];
+extern const unsigned char gImage_speed_1_c0[4248];
+extern const unsigned char gImage_speed_2_c0[5096];
+extern const unsigned char gImage_speed_3_c0[4566];
+extern const unsigned char gImage_speed_4_c0[5414];
+extern const unsigned char gImage_speed_5_c0[4884];
+extern const unsigned char gImage_speed_6_c0[5202];
+extern const unsigned char gImage_speed_7_c0[4884];
+extern const unsigned char gImage_speed_8_c0[5202];
+extern const unsigned char gImage_speed_9_c0[5202];
+
+extern const unsigned char gImage_speed_0_c1[5414];
+extern const unsigned char gImage_speed_1_c1[4248];
+extern const unsigned char gImage_speed_2_c1[5096];
+extern const unsigned char gImage_speed_3_c1[4566];
+extern const unsigned char gImage_speed_4_c1[5414];
+extern const unsigned char gImage_speed_5_c1[4884];
+extern const unsigned char gImage_speed_6_c1[5202];
+extern const unsigned char gImage_speed_7_c1[4884];
+extern const unsigned char gImage_speed_8_c1[5202];
+extern const unsigned char gImage_speed_9_c1[5202];
+
+extern const unsigned char gImage_speed_0_c2[5414];
+extern const unsigned char gImage_speed_1_c2[4248];
+extern const unsigned char gImage_speed_2_c2[5096];
+extern const unsigned char gImage_speed_3_c2[4566];
+extern const unsigned char gImage_speed_4_c2[5414];
+extern const unsigned char gImage_speed_5_c2[4884];
+extern const unsigned char gImage_speed_6_c2[5202];
+extern const unsigned char gImage_speed_7_c2[4884];
+extern const unsigned char gImage_speed_8_c2[5202];
+extern const unsigned char gImage_speed_9_c2[5202];
+
+extern const unsigned char gImage_speed_0_c3[5414];
+extern const unsigned char gImage_speed_1_c3[4248];
+extern const unsigned char gImage_speed_2_c3[5096];
+extern const unsigned char gImage_speed_3_c3[4566];
+extern const unsigned char gImage_speed_4_c3[5414];
+extern const unsigned char gImage_speed_5_c3[4884];
+extern const unsigned char gImage_speed_6_c3[5202];
+extern const unsigned char gImage_speed_7_c3[4884];
+extern const unsigned char gImage_speed_8_c3[5202];
+extern const unsigned char gImage_speed_9_c3[5202];
+
+extern const unsigned char gImage_speed_0_c4[5414];
+extern const unsigned char gImage_speed_1_c4[4248];
+extern const unsigned char gImage_speed_2_c4[5096];
+extern const unsigned char gImage_speed_3_c4[4566];
+extern const unsigned char gImage_speed_4_c4[5414];
+extern const unsigned char gImage_speed_5_c4[4884];
+extern const unsigned char gImage_speed_6_c4[5202];
+extern const unsigned char gImage_speed_7_c4[4884];
+extern const unsigned char gImage_speed_8_c4[5202];
+extern const unsigned char gImage_speed_9_c4[5202];
+
+extern const unsigned char gImage_speed_0_c5[5414];
+extern const unsigned char gImage_speed_1_c5[4248];
+extern const unsigned char gImage_speed_2_c5[5096];
+extern const unsigned char gImage_speed_3_c5[4566];
+extern const unsigned char gImage_speed_4_c5[5414];
+extern const unsigned char gImage_speed_5_c5[4884];
+extern const unsigned char gImage_speed_6_c5[5202];
+extern const unsigned char gImage_speed_7_c5[4884];
+extern const unsigned char gImage_speed_8_c5[5202];
+extern const unsigned char gImage_speed_9_c5[5202];
+
+extern const unsigned char gImage_speed_0_c6[5414];
+extern const unsigned char gImage_speed_1_c6[4248];
+extern const unsigned char gImage_speed_2_c6[5096];
+extern const unsigned char gImage_speed_3_c6[4566];
+extern const unsigned char gImage_speed_4_c6[5414];
+extern const unsigned char gImage_speed_5_c6[4884];
+extern const unsigned char gImage_speed_6_c6[5202];
+extern const unsigned char gImage_speed_7_c6[4884];
+extern const unsigned char gImage_speed_8_c6[5202];
+extern const unsigned char gImage_speed_9_c6[5202];
+
+extern const unsigned char gImage_speed_0_c7[5414];
+extern const unsigned char gImage_speed_1_c7[4248];
+extern const unsigned char gImage_speed_2_c7[5096];
+extern const unsigned char gImage_speed_3_c7[4566];
+extern const unsigned char gImage_speed_4_c7[5414];
+extern const unsigned char gImage_speed_5_c7[4884];
+extern const unsigned char gImage_speed_6_c7[5202];
+extern const unsigned char gImage_speed_7_c7[4884];
+extern const unsigned char gImage_speed_8_c7[5202];
+extern const unsigned char gImage_speed_9_c7[5202];
+
+extern const unsigned char gImage_speed_0_c8[5414];
+extern const unsigned char gImage_speed_1_c8[4248];
+extern const unsigned char gImage_speed_2_c8[5096];
+extern const unsigned char gImage_speed_3_c8[4566];
+extern const unsigned char gImage_speed_4_c8[5414];
+extern const unsigned char gImage_speed_5_c8[4884];
+extern const unsigned char gImage_speed_6_c8[5202];
+extern const unsigned char gImage_speed_7_c8[4884];
+extern const unsigned char gImage_speed_8_c8[5202];
+extern const unsigned char gImage_speed_9_c8[5202];
+
+extern const unsigned char gImage_speed_0_c9[5414];
+extern const unsigned char gImage_speed_1_c9[4248];
+extern const unsigned char gImage_speed_2_c9[5096];
+extern const unsigned char gImage_speed_3_c9[4566];
+extern const unsigned char gImage_speed_4_c9[5414];
+extern const unsigned char gImage_speed_5_c9[4884];
+extern const unsigned char gImage_speed_6_c9[5202];
+extern const unsigned char gImage_speed_7_c9[4884];
+extern const unsigned char gImage_speed_8_c9[5202];
+extern const unsigned char gImage_speed_9_c9[5202];
+
+extern const unsigned char gImage_speed_0_c10[5414];
+extern const unsigned char gImage_speed_1_c10[4248];
+extern const unsigned char gImage_speed_2_c10[5096];
+extern const unsigned char gImage_speed_3_c10[4566];
+extern const unsigned char gImage_speed_4_c10[5414];
+extern const unsigned char gImage_speed_5_c10[4884];
+extern const unsigned char gImage_speed_6_c10[5202];
+extern const unsigned char gImage_speed_7_c10[4884];
+extern const unsigned char gImage_speed_8_c10[5202];
+extern const unsigned char gImage_speed_9_c10[5202];
+
